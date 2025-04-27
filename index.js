83bf38a85cb7034b80792d3917d9f139d8ffc481
José Evangelista da Silva Filho <juniornyanata@gmail.com>
2025-04-27 13:48:37 -0300

    primeiro

---
+++ index.js
@@ -0,0 +1,16 @@
+cep.onkeyup = () => {
+    if (cep.value.length == 8) {
+        console.log("Digitei o CEP");
+        fetch(`https://viacep.com.br/ws/${cep.value}/json/`)
+            .then(resposta => resposta.json())
+            .then(resposta2 => {
+                rua.value = resposta2.logradouro;
+                bairro.value = resposta2.bairro;
+                cidade.value = resposta2.localidade;
+                uf.value = resposta2.uf;
+            })
+            .finally(() => {
+                alert("Concluído");
+            })
+    }
+}
